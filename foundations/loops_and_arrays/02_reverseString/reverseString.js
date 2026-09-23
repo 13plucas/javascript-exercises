ac936d1d5f481b665339a031591608e792c6dc93
Philip Lucas <306472276+13plucas@users.noreply.github.com>
2026-09-23 21:05:50 +0100

    Complete loops and arrays second exercise

--- foundations/loops_and_arrays/02_reverseString/reverseString.js
+++ foundations/loops_and_arrays/02_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let letters = [...str];
+    letters.reverse();
+    return letters.join('');
 };
+
+console.log(reverseString("Hello"));
 
 // Do not edit below this line
 module.exports = reverseString;
