8248db119f1eaeedf490eeaea555214d10d23c6c
Philip Lucas <306472276+13plucas@users.noreply.github.com>
2026-09-23 21:55:06 +0100

    Complete loops and arrays fourth exercise

--- foundations/loops_and_arrays/04_sumAll/sumAll.js
+++ foundations/loops_and_arrays/04_sumAll/sumAll.js
@@ -1,6 +1,18 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
+    let arr = [];
 
+    for (let i = firstNum; i <= secondNum; i++) {
+        arr.push(i);
+    }
+
+    addAll = arr.reduce((total, currentItem) => {
+        return total + currentItem;
+    }, 0);
+
+    console.log(addAll);
 };
+
+sumAll(1, 4);
 
 // Do not edit below this line
 module.exports = sumAll;
