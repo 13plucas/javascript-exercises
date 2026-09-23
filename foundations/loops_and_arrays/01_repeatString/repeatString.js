b0d9c4fa6b98380967e79e5a95fb545be6a5ff3b
Philip Lucas <306472276+13plucas@users.noreply.github.com>
2026-09-23 17:23:38 +0100

    Complete loops and arrays first exercise

--- foundations/loops_and_arrays/01_repeatString/repeatString.js
+++ foundations/loops_and_arrays/01_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
+const repeatString = function(word, num) {
+    if (num < 0) return "ERROR";
 
+    let string = "";
+    
+    let i = 0;
+    while (i < num) {
+        string += word;
+        i++;
+    }
+    
+    return string;
 };
+
+console.log(repeatString("Hey", 3));
 
 // Do not edit below this line
 module.exports = repeatString;
