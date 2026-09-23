2b62520da16af4a3bd6086f3b300aa1051b55ed1
Philip Lucas <306472276+13plucas@users.noreply.github.com>
2026-09-23 21:21:43 +0100

    Complete loops and arrays third exercise

--- foundations/loops_and_arrays/03_removeFromArray/removeFromArray.js
+++ foundations/loops_and_arrays/03_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, item) {
+
+    function isNotItem(num) {
+        return num !== item
+    }
+
+    let removed = arr.filter(isNotItem);
+    console.log(removed);
 };
+
+removeFromArray([1, 2, 2, 4, 5], 2);
+
+removeFromArray([5, 5, 2, 3, 3, 4, 3, 8, 2], 3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
