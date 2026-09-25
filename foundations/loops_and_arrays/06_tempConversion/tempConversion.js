dbbc0c9bb4987964de658074ce52875a62961ecc
Philip Lucas <306472276+13plucas@users.noreply.github.com>
2026-09-25 14:54:37 +0100

    Complete loops and arrays sixth exercise

--- foundations/loops_and_arrays/06_tempConversion/tempConversion.js
+++ foundations/loops_and_arrays/06_tempConversion/tempConversion.js
@@ -1,8 +1,22 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInFahrenheit) {
+  let celcius = (tempInFahrenheit - 32) * (5 / 9);
+  let rounded = Math.round(celcius * 10) / 10;
+  console.log(rounded);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInCelcius) {
+  let fahrenheit = (tempInCelcius * 9/5 + 32);
+  let rounded = Math.round(fahrenheit * 10) / 10;
+  console.log(rounded);
 };
+
+convertToCelsius(32);
+convertToCelsius(100);
+convertToCelsius(-100);
+
+convertToFahrenheit(0);
+convertToFahrenheit(73.2);
+convertToFahrenheit(-10);
 
 // Do not edit below this line
 module.exports = {
