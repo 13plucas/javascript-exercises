133579143cb5885be788b937a86cfb194367220f
Philip Lucas <306472276+13plucas@users.noreply.github.com>
2026-09-25 14:32:09 +0100

    Complete loops and arrays fifth exercise

--- foundations/loops_and_arrays/05_leapYears/leapYears.js
+++ foundations/loops_and_arrays/05_leapYears/leapYears.js
@@ -1,6 +1,22 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    const isDivisibleByFour = year % 4 === 0;
+    const isDivisibleByOneHundred = year % 100 === 0;
+    const isDivisibleByFourHundred = year % 400 === 0;
 
+   if (isDivisibleByOneHundred && !isDivisibleByFourHundred || !isDivisibleByFour) {
+    return (`${year} is not a leap year`);
+   } else {
+    return (`${year} is a leap year`);
+   }
 };
+
+console.log(leapYears(34992));
+
+console.log(leapYears(1900));
+
+console.log(leapYears(1600));
+
+console.log(leapYears(700));
 
 // Do not edit below this line
 module.exports = leapYears;
